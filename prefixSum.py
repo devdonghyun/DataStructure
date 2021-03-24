7c7530fbf020cc1762090b508569dd55b79df857
devDongHYun <ehdgus8725@naver.com>
2021-03-24 23:40:15 +0900

    Solve prefixSum

--- prefixSum.py
+++ prefixSum.py
@@ -7,19 +7,26 @@ def prefixSum1(X, n):
     for i in range(n):
         S[i] = 0
         for j in range(i):
-            S[i] += A[j]
+            S[i] += X[j]
 
 
 def prefixSum2(X, n):
     # code for prefixSum2
-    S[0] = A[0]
+    S[0] = X[0]
     for i in range(n):
-        S[i] = S[i-1] + A[i]
+        S[i] = S[i-1] + X[i]
 
 
 random.seed()		# random 함수 초기화
 # n 입력받음
+n = int(input())
 # 리스트 X를 randint를 호출하여 n개의 랜덤한 숫자로 채움
+X, S = [], []
+for i in range(n):
+    X.append(random.randint(-n, n))
+
 # prefixSum1 호출
+prefixSum1(X, n)
 # prefixSum2 호출
+prefixSum2(X, n)
 # 두 함수의 수행시간 출력
