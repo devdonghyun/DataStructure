3bc760a5f109e26141d0b8b400c63fbb06147648
devDongHYun <ehdgus8725@naver.com>
2021-03-27 23:11:43 +0900

    Solving twoDimensionalSum

--- twoDimensionalSum.py
+++ twoDimensionalSum.py
@@ -4,8 +4,10 @@ import random
 
 def sum(A, n):
     # code here
+    for i in range(n):
+        for j in range(n):
 
-    # n = 1 이상 5000 이하 정수 값 입력
-    # 리스트 A에 랜덤 정수 값 n개 채움
-    # sum 함수 호출 + 시간 측정
-    # 함수의 수행시간을 출력
+            # n = 1 이상 5000 이하 정수 값 입력
+            # 리스트 A에 랜덤 정수 값 n개 채움
+            # sum 함수 호출 + 시간 측정
+            # 함수의 수행시간을 출력
